da362c36d87cfe415e47b35c78ef8ad77dae9c96
UMAID-AKHTAR <umaidakhtar933@gmail.com>
2026-01-28 23:34:14 +0530

    reverse number

--- ARRAY/ReverseNumber.cpp
+++ ARRAY/ReverseNumber.cpp
@@ -1,11 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-int main(){
-    int num=123;
-    while(num!=0){
-        int n=num%10;
-        cout<<n;
-        num=num/10;
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            // Overflow check before multiplication
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
+                return 0;
+
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+                return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
     }
+};
+
+int main() {
+    Solution obj;
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+    int result = obj.reverse(x);
+    cout << "Reversed integer: " << result << endl;
+    return 0;
 }
